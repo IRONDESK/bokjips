211c960c8fefa59a704fe47cae552c21c7f767d1
IRONDESK <zerozerobase@gmail.com>
2022-11-09 14:09:33 +0900

    [Feat] 사이트 Header 구현

--- src/components/layouts/partials/Header.tsx
+++ src/components/layouts/partials/Header.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { COLOR } from "../../../constants/style";
+import SearchBar from "../../SearchBar";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Logo>
+        <strong>복지</strong>편살
+      </Logo>
+      <SearchBar />
+      <User></User>
+    </Container>
+  );
 }
+
+const Container = styled.header`
+  display: flex;
+  padding: 24px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  font-family: "GangwonEdu";
+  font-size: 2rem;
+  strong {
+    color: ${COLOR.main};
+  }
+`;
+
+const User = styled.button`
+  width: 24px;
+  height: 24px;
+  background-image: url("/icons/face.svg");
+  background-size: 24px;
+  background-position: center;
+`;
 
 export default Header;
--- src/constants/style.tsx
+++ src/constants/style.tsx
@@ -1,4 +1,5 @@
 export const COLOR = {
+  main: "#006ae4",
   report: "#ff3d00",
   check: "#4dce2d",
 };
