b382ef0c4b51957dccaa79f090731044c1be2b76
IRONDESK <zerozerobase@gmail.com>
2022-11-10 20:36:51 +0900

    [Chore] 스타일 세팅 수정

--- src/constants/style.tsx
+++ src/constants/style.tsx
@@ -3,3 +3,8 @@ export const COLOR = {
   report: "#ff3d00",
   check: "#4dce2d",
 };
+
+export const SHADOW = {
+  basic: "0 0 24px 8px rgba(0, 0, 0, 0.05)",
+  hover: "0 0 32px 12px rgba(0, 0, 0, 0.05)",
+};
