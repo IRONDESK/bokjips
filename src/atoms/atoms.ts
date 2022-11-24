2eb1a8ea843ebc2a943d796b3a53d6128ac6e92d
IRONDESK <zerozerobase@gmail.com>
2022-11-24 15:17:06 +0900

    [Package] Jotai 상태 관리 패키지 설치

---
+++ src/atoms/atoms.ts
@@ -0,0 +1,14 @@
+import { atom } from "jotai";
+
+interface keyFilterType {
+  keyword: string;
+  industry: string;
+}
+
+export const selectedModal = atom<boolean>(false);
+export const keyFilter = atom<keyFilterType>({
+  keyword: "",
+  industry: "",
+});
+export const wageFilter = atom<number>(0);
+export const selectedFilter = atom<string[]>([]);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "jotai";
 import { Layout } from "../components/Layouts/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
