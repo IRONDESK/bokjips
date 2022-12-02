68d0b93ff1b71d0efd6a8009f4ddccb743d7fd75
IRONDESK <zerozerobase@gmail.com>
2022-12-02 10:40:30 +0900

    [Chore] 유저정보 타입 파일

---
+++ src/types/UserData.ts
@@ -0,0 +1,11 @@
+export interface IUserDataTypes {
+  username: string;
+  password: string;
+  nickname: string;
+  job: string;
+}
+
+export interface IUserLoginDataTypes {
+  memberId: string;
+  password: string;
+}
