81495661dc2a93df3510b8d8a922a147ad21bf84
IRONDESK <zerozerobase@gmail.com>
2022-12-28 22:27:26 +0900

    [Feat] 로딩 화면 페이지 생성

---
+++ src/components/Layouts/Loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function Loading() {
+  return <Container>Loading...</Container>;
+}
+const Container = styled.section`
+  padding: 80px 0;
+  height: 30vh;
+  font-size: 1.4rem;
+  text-align: center;
+`;
+
+export default Loading;
