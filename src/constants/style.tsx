a6844e4f38770895f6b95e67f8d2ca30ab8419a5
IRONDESK <zerozerobase@gmail.com>
2023-09-09 23:49:33 +0900

    [Chore] 색상 추가, 기본 폰트 변경

--- src/constants/style.tsx
+++ src/constants/style.tsx
@@ -1,9 +1,14 @@
 export const COLOR = {
-  main: "#5ec9cb",
-  mainLight: "#b8ebec",
+  mainDark: "#21b993",
+  main: "#22d0a4",
+  mainLight: "#b4ecde",
+  mainLight1: "#e4fff8",
   report: "#ff3d00",
   check: "#4dce2d",
+  lightgray1: "#f2f2f2",
   lightgray: "#e9e9e9",
+  gray1: "#C1C1C2",
+  gray: "#767678",
 };
 
 export const SHADOW = {
