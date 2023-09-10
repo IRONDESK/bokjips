8c1f96492098271f7e275161b609586675eb201e
IRONDESK <zerozerobase@gmail.com>
2023-09-10 21:28:12 +0900

    [Refactor] 회사 페이지 내 상단 배너 개선

--- src/components/Corp/Banner.tsx
+++ src/components/Corp/Banner.tsx
@@ -12,6 +12,7 @@ import { COMPANY_TYPES_LITERAL } from "../../constants/job";
 import { ServerURL } from "../../api/ServerURL";
 import { HandlerCompanyFavorite } from "../../api/CompanyApi";
 import { ICompanyDataTypes } from "../../types/CompanyData";
+import { VerifiedIcon } from "../../svg/CardIcons";
 
 interface ICorpBannerPropsType {
   corpId: string;
@@ -44,37 +45,29 @@ function Banner({ corpId, companyData }: ICorpBannerPropsType) {
   return (
     <Container>
       <div className="corp-identified">
-        <img src={companyData?.logo} alt={companyData?.name} />
-        <div>
-          <h1 id="corp-name">{companyData?.name}</h1>
-          {companyData?.isCertified === "true" && (
-            <i>
-              <span className="a11y-hidden">현직자 검증 정보</span>
-            </i>
-          )}
-        </div>
-        <span className="corp-category">
-          <span>{companyData?.isPublicStock ? "상장" : "비상장"}</span>
-          <span>{COMPANY_TYPES_LITERAL[companyData?.classification]}</span>
-        </span>
+        <img className="corp-logo-image" src={companyData?.logo} alt={companyData?.name} />
+        <NameBox>
+          <div className="corp-title">
+            <h1 className="corp-name">{companyData?.name}</h1>
+            <p className="verified-icon">{companyData?.isCertified === "true" && <VerifiedIcon width={24} />}</p>
+          </div>
+          <div className="corp-category">
+            <span>{companyData?.isPublicStock ? "상장" : "비상장"}</span>
+            <span>{COMPANY_TYPES_LITERAL[companyData?.classification]}</span>
+          </div>
+        </NameBox>
       </div>
       <div className="corp-buttons">
         <Button
           id="company-favorite-button"
-          aria-label={`찜 등록 및 해제 버튼. ${
-            companyData?.favorite || 0
-          }명이 찜했습니다.`}
+          aria-label={`찜 등록 및 해제 버튼. ${companyData?.favorite || 0}명이 찜했습니다.`}
           icon={!!companyData?.isFavorite ? "heart_white_fill" : "heart"}
           isFavorite={!!companyData?.isFavorite}
           onClick={handlerFavorite}
         >
           {companyData?.favorite?.toLocaleString() || 0}
         </Button>
-        <Button
-          icon="site"
-          aria-label="회사 사이트로 이동"
-          onClick={() => router.push(companyData?.site || "")}
-        >
+        <Button icon="site" aria-label="회사 사이트로 이동" onClick={() => router.push(companyData?.site || "")}>
           사이트
         </Button>
         <Button
@@ -89,55 +82,111 @@ function Banner({ corpId, companyData }: ICorpBannerPropsType) {
   );
 }
 
+const NameBox = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  .corp-title {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    .corp-name {
+      font-weight: 600;
+      font-size: 1.5rem;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .verified-icon {
+      cursor: help;
+      position: relative;
+      display: inline-flex;
+      align-items: center;
+      &:hover::after {
+        content: "회사 현직자가 확인한 복지 정보";
+        position: absolute;
+        display: inline-block;
+        padding: 6px 8px;
+        width: max-content;
+        top: 30px;
+        left: -4px;
+        background-color: #fff;
+        border: 1px solid ${COLOR.gray1};
+        border-radius: 4px;
+        color: ${COLOR.gray};
+        font-size: 0.9rem;
+        z-index: 1;
+      }
+      &:hover::before {
+        content: "";
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        top: 26px;
+        left: 8px;
+        background-color: #fff;
+        border-top: 1px solid ${COLOR.gray1};
+        border-left: 1px solid ${COLOR.gray1};
+        transform: rotate(45deg);
+        z-index: 2;
+      }
+    }
+  }
+  .corp-category {
+    white-space: nowrap;
+    font-size: 0.95rem;
+    opacity: 0.65;
+    span {
+      margin: 0 4px;
+    }
+  }
+  @media (max-width: 580px) {
+    gap: 6px;
+    flex-direction: column;
+    align-items: flex-start;
+
+    .corp-category {
+      display: inline-flex;
+      gap: 10px;
+      span {
+        margin: 0;
+      }
+    }
+    .verified-icon {
+      cursor: default !important;
+      overflow: hidden;
+      &:hover::before,
+      &:hover::after {
+        content: "";
+        display: none;
+      }
+    }
+  }
+`;
+
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin: 20px 0;
+  margin: 0 -24px 16px;
+  padding: 20px 32px;
+  background-color: ${COLOR.lightgray};
   .corp-identified {
     display: flex;
     align-items: center;
     padding: 0 8px;
-    gap: 8px;
-    img {
+    gap: 12px;
+    .corp-logo-image {
       float: left;
-      width: 48px;
-      height: 48px;
-      border: 1px solid #e4e4e4;
-      border-radius: 12px;
-      background-color: #fff;
+      width: 68px;
+      height: 52px;
+      border-radius: 8px;
+      background-color: #000;
       object-fit: cover;
-    }
-    div {
-      display: flex;
-      gap: 2px;
-      align-items: center;
-      #corp-name {
-        font-weight: 600;
-        font-size: 1.5rem;
-        white-space: nowrap;
-        text-overflow: ellipsis;
-        overflow: hidden;
-      }
-      i {
-        display: inline-block;
-        width: 20px;
-        height: 20px;
-        background-image: url("/icons/verified.svg");
-        background-size: 20px;
-      }
-    }
-    .corp-category {
-      white-space: nowrap;
-      font-size: 0.95rem;
-      opacity: 0.65;
-      span {
-        margin: 0 4px;
-      }
     }
   }
   @media (max-width: 840px) {
-    margin: 12px 0 20px;
+    margin: 0 -24px 16px;
     flex-direction: column;
     .corp-identified {
       width: 100%;
@@ -161,9 +210,10 @@ const Container = styled.div`
     }
   }
   @media (max-width: 580px) {
+    padding: 24px 12px 20px;
+    margin: 0 -8px 16px;
     .corp-identified {
-      padding: 8px 12px 20px;
-      flex-direction: column;
+      padding: 0 12px 20px;
     }
   }
 `;
@@ -171,15 +221,15 @@ const Container = styled.div`
 const Button = styled.button<{ icon: string; isFavorite?: boolean }>`
   position: relative;
   margin: 0 12px 0 0;
-  padding: 12px 16px 12px 42px;
-  min-width: 80px;
+  padding: 12px 14px 12px 38px;
+  min-width: 72px;
   background-color: #fff;
-  border-radius: 28px;
-  box-shadow: ${SHADOW.basic};
-  background-color: ${(props) => (props.isFavorite ? COLOR.main : "none")};
-  color: ${(props) => (props.isFavorite ? "#fff" : "none")};
-  font-size: 0.95rem;
-  font-weight: ${(props) => (props.isFavorite ? "500" : "none")};
+  border-radius: 16px;
+  border: 1px solid ${({ isFavorite }) => (isFavorite ? COLOR.mainDark : COLOR.lightgray)};
+  background-color: ${({ isFavorite }) => (isFavorite ? COLOR.main : "none")};
+  color: ${({ isFavorite }) => (isFavorite ? "#fff" : "none")};
+  font-size: 0.9rem;
+  font-weight: ${({ isFavorite }) => (isFavorite ? "500" : "none")};
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
@@ -189,7 +239,7 @@ const Button = styled.button<{ icon: string; isFavorite?: boolean }>`
     position: absolute;
     width: 20px;
     height: 20px;
-    left: 18px;
+    left: 13px;
     top: 50%;
     background-image: url(${(props) => `/icons/${props.icon}.svg`});
     background-size: 18px;
@@ -198,7 +248,7 @@ const Button = styled.button<{ icon: string; isFavorite?: boolean }>`
     transform: translateY(-50%);
   }
   &:hover {
-    box-shadow: ${SHADOW.hover};
+    filter: brightness(1.1);
   }
   @media (max-width: 840px) {
     margin: 0;
--- src/pages/corp/[id].tsx
+++ src/pages/corp/[id].tsx
@@ -20,114 +20,102 @@ import { ServerURL } from "../../api/ServerURL";
 import { swrFetcher } from "../../api/MyInfoApi";
 
 interface ICorpPropsType {
-    corpId?: string;
-    companyName?: string;
+  corpId?: string;
+  companyName?: string;
 }
 
 function CorpId({ corpId, companyName }: ICorpPropsType) {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    const cookie = getCookie("accessToken") as string;
-    const [isSplited, setIsSplited] = useAtom(verticalSplited);
+  const cookie = getCookie("accessToken") as string;
+  const [isSplited, setIsSplited] = useAtom(verticalSplited);
 
-    const {
-        data: companyData,
-        error: companyError,
-        isValidating,
-    } = useSWR([`${ServerURL}/${corpId}`, cookie], swrFetcher, {
-        revalidateOnFocus: false,
-    });
+  const {
+    data: companyData,
+    error: companyError,
+    isValidating,
+  } = useSWR([`${ServerURL}/${corpId}`, cookie], swrFetcher, {
+    revalidateOnFocus: false,
+  });
 
-    const { data: roleData, error: roleError } = useSWR(
-        [`${ServerURL}/userRole`, cookie],
-        swrFetcher,
-        {
-            revalidateOnFocus: false,
-            onErrorRetry: (error) => {
-                if (error.status === 401) return;
-            },
-        }
-    );
+  const { data: roleData, error: roleError } = useSWR([`${ServerURL}/userRole`, cookie], swrFetcher, {
+    revalidateOnFocus: false,
+    onErrorRetry: (error) => {
+      if (error.status === 401) return;
+    },
+  });
 
-    const dateConvert = (target: Date | undefined) => {
-        return new Intl.DateTimeFormat("ko-KR", {
-            dateStyle: "long",
-            timeStyle: "medium",
-        }).format(target || 0);
-    };
+  const dateConvert = (target: Date | undefined) => {
+    return new Intl.DateTimeFormat("ko-KR", {
+      dateStyle: "long",
+      timeStyle: "medium",
+    }).format(target || 0);
+  };
 
-    return (
-        <>
-            <Title title={companyName} />
-            {!isValidating && companyData ? (
-                <Container isSplited={isSplited}>
-                    <SideOne>
-                        <Banner
-                            corpId={corpId as string}
-                            companyData={companyData}
-                        />
-                        <Detail
-                            wage={companyData?.wage}
-                            isInclusiveWage={companyData?.isInclusiveWage}
-                            workingConditions={companyData?.workingConditions}
-                            workSupport={companyData?.workSupport}
-                            offDutySupport={companyData?.offDutySupport}
-                            officeEnvironment={companyData?.officeEnvironment}
-                        />
-                        <UpdateAtText>
-                            이 회사의 복지 정보는{" "}
-                            {dateConvert(companyData?.updateAt)} 수정되었습니다.
-                        </UpdateAtText>
-                        {roleData?.roles === "ROLE_ADMIN" && (
-                            <EditButtons companyId={corpId as string} />
-                        )}
-                    </SideOne>
-                    <SideTwo isSplited={isSplited}>
-                        <Vertical
-                            type="button"
-                            id="vertical-active"
-                            aria-label="좌우 화면 분리 버튼"
-                            onClick={() => setIsSplited(!isSplited)}
-                        ></Vertical>
-                        <Comments corpId={corpId as string} />
-                    </SideTwo>
-                </Container>
-            ) : !isValidating && companyError ? (
-                <NoData code={companyError?.response?.status} />
-            ) : (
-                <Loading />
-            )}
-        </>
-    );
+  return (
+    <>
+      <Title title={companyName} />
+      {!isValidating && companyData ? (
+        <Container isSplited={isSplited}>
+          <SideOne>
+            <Banner corpId={corpId as string} companyData={companyData} />
+            <Detail
+              wage={companyData?.wage}
+              isInclusiveWage={companyData?.isInclusiveWage}
+              workingConditions={companyData?.workingConditions}
+              workSupport={companyData?.workSupport}
+              offDutySupport={companyData?.offDutySupport}
+              officeEnvironment={companyData?.officeEnvironment}
+            />
+            <UpdateAtText>이 회사의 복지 정보는 {dateConvert(companyData?.updateAt)} 수정되었습니다.</UpdateAtText>
+            {roleData?.roles === "ROLE_ADMIN" && <EditButtons companyId={corpId as string} />}
+          </SideOne>
+          <SideTwo isSplited={isSplited}>
+            <Vertical
+              type="button"
+              id="vertical-active"
+              aria-label="좌우 화면 분리 버튼"
+              onClick={() => setIsSplited(!isSplited)}
+            ></Vertical>
+            <Comments corpId={corpId as string} />
+          </SideTwo>
+        </Container>
+      ) : !isValidating && companyError ? (
+        <NoData code={companyError?.response?.status} />
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
 
 const Container = styled.main<{ isSplited: boolean }>`
-    padding: 0 24px;
-    display: ${(props) => (props.isSplited ? "flex" : "block")};
-    justify-content: space-between;
-    @media (max-width: 1125px) {
-        display: block;
-    }
-    @media (max-width: 580px) {
-        padding: 0 8px 20px;
-    }
+  padding: 0 24px;
+  display: ${(props) => (props.isSplited ? "flex" : "block")};
+  justify-content: space-between;
+  @media (max-width: 1125px) {
+    display: block;
+  }
+  @media (max-width: 580px) {
+    padding: 0 8px 20px;
+  }
 `;
 const SideOne = styled.section`
-    flex: 1;
+  flex: 1;
 `;
 const SideTwo = styled.section<{ isSplited: boolean }>`
-    ${(props) =>
-        !props.isSplited &&
-        `
+  ${(props) =>
+    !props.isSplited &&
+    `
   margin: 36px 0 0;
   padding: 36px 0;
   border-top: 1px solid #c8c8c8;`}
-    @media (min-width: 1125px) {
-        ${(props) =>
-            props.isSplited &&
-            `
+  @media (min-width: 1125px) {
+    ${(props) =>
+      props.isSplited &&
+      `
   position: sticky;
   top: 0;
   margin: 0 -24px 0 0;
@@ -140,55 +128,53 @@ const SideTwo = styled.section<{ isSplited: boolean }>`
     margin-right: 24px;
   }
 `}
-    }
+  }
 
-    @media (max-width: 1125px) {
-        margin: 36px 0 0;
-        padding: 36px 0;
-        border-top: 1px solid #c8c8c8;
-    }
+  @media (max-width: 1125px) {
+    margin: 36px 0 0;
+    padding: 36px 0;
+    border-top: 1px solid #c8c8c8;
+  }
 `;
 
 const UpdateAtText = styled.p`
-    margin: 8px 0;
-    font-size: 0.65rem;
-    text-align: center;
-    opacity: 0.5;
+  margin: 8px 0;
+  font-size: 0.65rem;
+  text-align: center;
+  opacity: 0.5;
 `;
 
 const Vertical = styled.button`
-    position: absolute;
-    right: 0;
-    margin: -12px 40px 0 0;
-    padding: 20px;
-    background-color: #fff;
-    background-image: url("/icons/vertical_split.svg");
-    background-position: center;
-    background-repeat: no-repeat;
-    border-radius: 28px;
-    transition: all 0.3s;
-    box-shadow: ${SHADOW.basic};
-    &:hover {
-        box-shadow: ${SHADOW.hover};
-    }
-    @media (max-width: 1125px) {
-        visibility: hidden;
-        opacity: 0;
-    }
+  position: absolute;
+  right: 0;
+  margin: -12px 40px 0 0;
+  padding: 20px;
+  background-color: #fff;
+  background-image: url("/icons/vertical_split.svg");
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 28px;
+  transition: all 0.3s;
+  box-shadow: ${SHADOW.basic};
+  &:hover {
+    box-shadow: ${SHADOW.hover};
+  }
+  @media (max-width: 1125px) {
+    visibility: hidden;
+    opacity: 0;
+  }
 `;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { id } = context.query;
-    const [companyData] = await Promise.all([
-        swrFetcher(`${ServerURL}/${id}`, ""),
-    ]);
+  const { id } = context.query;
+  const [companyData] = await Promise.all([swrFetcher(`${ServerURL}/${id}`, "")]);
 
-    return {
-        props: {
-            corpId: id,
-            companyName: companyData.name,
-        },
-    };
+  return {
+    props: {
+      corpId: id,
+      companyName: companyData.name,
+    },
+  };
 };
 
 export default CorpId;
